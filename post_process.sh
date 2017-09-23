163c5102c14261303b542298e6f04496d81dc11f
themightyoarfish <rdiederichse@uos.de>
2017-09-23 10:39:07 +0200

    don't rekey

--- post_process.sh
+++ post_process.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 for f in *.bib; do
-    command="bibtool -s $f -k -o $f"
+    # don't use -k to rekey here since I changed a few keys to disambiguate
+    # publications. See git changelog for which ones.
+    command="bibtool -s $f -o $f"
     echo "Prettifying $f with $command..."
     $command
 done
